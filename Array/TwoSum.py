b4f9a4c5f5b0edf972868a37dd7cda48d81833e8
Gabriel Campanelli Iamato <114399483+GabrielIamato@users.noreply.github.com>
2025-09-01 21:22:49 -0300

    - Solution

---
+++ Array/TwoSum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mapa = {}
+        for i, valor in enumerate(nums):
+            # Calcula o complemento do target
+            complemento = target - valor
+            # Se o complemento já foi passado, retorna o índice dele
+            if complemento in mapa:
+                # mapa[complemento] retorna o índice de "complemento"
+                return[mapa[complemento], i] 
+            # Se não achou nos valores já percorridos, insere em mapa
+            mapa[valor] = i
